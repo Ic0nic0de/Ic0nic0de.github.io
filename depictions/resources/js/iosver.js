e7d3a10b3998703169c2b8e89f03294aa14e7d43
Ic0nic0de <Christian.hawkes90@gmail.com>
2018-08-05 17:22:29 +0100

    p

--- depictions/resources/js/iosver.js
+++ depictions/resources/js/iosver.js
@@ -1,78 +1,33 @@
-var VERSION_CHECK_SUPPORTED = "Your iOS version is supported! &#x1f60a;";
-var VERSION_CHECK_NEEDS_UPGRADE = "Requires at least iOS %s &#x1f615;";
-var VERSION_CHECK_UNCONFIRMED = "Not yet tested on iOS %s &#x1f601;";
-var VERSION_CHECK_UNSUPPORTED = "Only compatible with iOS %s to %s &#x1f61e;";
+//Thank you Matchstic, you beautiful person
+const VERSION_CHECK_SUPPORTED = "<p>iOS %s is supported. ✓</p>";
+const VERSION_CHECK_UNSUPPORTED = "<p>iOS %s is not supported. ✕</p>";
+const VERSION_CHECK_UNCONFIRMED = "<p>iOS %s has not been tested!</p>";
 
-function iOSversion(minIOS,maxIOS,otherIOS,callBack) {
-	"use strict";
-
-
-	function parseVersionString(version) {
-		var bits = version.split(".");
-		return [
-				parseInt(bits[0], 10),
-				parseInt(bits[1] ? bits[1] : 0, 10),
-				parseInt(bits[2] ? bits[2] : 0, 10)
-			   ];
+function iOSversion() {
+	if (/iP(hone|od|ad)/.test(navigator.platform)) {
+		var v = (navigator.appVersion).match(/OS (\d+)_(\d+)_?(\d+)?/);
+		return [parseInt(v[1], 10), parseInt(v[2], 10), parseInt(v[3] || 0, 10)];
 	}
-
-	function compareVersions(one, two) {
-		// https://gist.github.com/TheDistantSea/8021359
-		for (var i = 0; i < one.length; ++i) {
-			if (two.length == i) {
-				return 1;
-			}
-
-			if (one[i] == two[i]) {
-				continue;
-			} else if (one[i] > two[i]) {
-				return 1;
-			} else {
-				return -1;
-			}
-		}
-
-		if (one.length != two.length) {
-			return -1;
-		}
-
+	else {
 		return 0;
 	}
-
-	var version = navigator.appVersion.match(/CPU( iPhone)? OS (\d+)_(\d+)(_(\d+))? like/i);
-	if (!version) {
-		return 0;
-	}
-
-	var osVersion = [
-						parseInt(version[2], 10),
-						parseInt(version[3], 10),
-						parseInt(version[4] ? version[5] : 0, 10)
-					],
-
-		osString = osVersion[0] + "." + osVersion[1] + (osVersion[2] && osVersion[2] != 0 ? "." + osVersion[2] : ""),
-		minString = minIOS,
-		maxString = maxIOS,
-
-		minVersion = parseVersionString(minString),
-		maxVersion = maxString ? parseVersionString(maxString) : null,
-
-		message = VERSION_CHECK_SUPPORTED,
-		isBad = false;
-
-	if (compareVersions(minVersion, osVersion) == 1) {
-		message = VERSION_CHECK_NEEDS_UPGRADE.replace("%s", minString);
-		isBad = true;
-	} else if (maxVersion && compareVersions(maxVersion, osVersion) == -1) {
-		if ("unsupported" == otherIOS) {
-			message = VERSION_CHECK_UNSUPPORTED.replace("%s", minString).replace("%s", maxString);
-		} else {
-			message = VERSION_CHECK_UNCONFIRMED.replace("%s", osString);
-		}
-
-		isBad = true;
-	}
-	callBack(message,isBad);
-
-	return (isBad?-1:1);
 }
+
+var maxVersion = document.getElementById('maxVersion').innerHTML;
+var minVersion = document.getElementById('minVersion').innerHTML;
+var version = iOSversion();
+var versionNumber = parseFloat(version[0] + "." + version[1]);
+
+	if(versionNumber >= minVersion && version <= maxVersion) {
+		document.getElementById('compatibility').innerHTML=VERSION_CHECK_SUPPORTED.replace("%s", versionNumber);
+		document.body.style.setProperty("--title-color", "rgba(39,174,96, 0.5)");
+	} else if(versionNumber > maxVersion) {
+		document.getElementById('compatibility').innerHTML=VERSION_CHECK_UNCONFIRMED.replace("%s", versionNumber);
+		document.body.style.setProperty("--title-color", "rgba(192,57,43, 0.5)");
+	} else if(!versionNumber) {
+		document.getElementById('compatibility').innerHTML="<p>Can't get device version.</p>";
+		document.body.style.setProperty("--title-color", "rgba(192,57,43, 0.5)");
+	} else {
+		document.getElementById('compatibility').innerHTML=VERSION_CHECK_UNSUPPORTED.replace("%s", versionNumber);
+		document.body.style.setProperty("--title-color", "rgba(192,57,43, 0.5)");
+	}
