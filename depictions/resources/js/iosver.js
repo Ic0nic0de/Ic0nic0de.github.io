927fdaacc241a2965fbe7cff934efcf27af1a2ff
Ic0nic0de <Christian.hawkes90@gmail.com>
2018-08-05 22:02:19 +0100

    p

--- depictions/resources/js/iosver.js
+++ depictions/resources/js/iosver.js
@@ -5,7 +5,7 @@ const VERSION_CHECK_UNCONFIRMED = "<p>iOS %s has not been tested!</p>";
 
 function iOSversion() {
 	if (/iP(hone|od|ad)/.test(navigator.platform)) {
-		var v = (navigator.appVersion).match(/OS (\d+)_(\d+)_?(\d+)?/),version;
+		var v = (navigator.appVersion).match(/OS (\d+)_(\d+)_?(\d+)?/);
 		return [parseInt(v[1], 10), parseInt(v[2], 10), parseInt(v[3] || 0, 10)];
 	}
 	else {
@@ -16,19 +16,18 @@ function iOSversion() {
 var maxVersion = document.getElementById('maxVersion').innerHTML;
 var minVersion = document.getElementById('minVersion').innerHTML;
 var version = iOSversion();
-var versionNumber = parseFloat(version[0] + "." + version[1]);
-var versionRepresentation = version[0] + "." + version[1] + "." + version[2];
+var versionNumber = String(version[0] + "." + version[1] + "." version[2]);
 
-if (versionNumber >= minVersion && version <= maxVersion) {
-		document.getElementById('compatibility').innerHTML=VERSION_CHECK_SUPPORTED.replace("%s", versionRepresentation);
+	if(versionNumber >= minVersion && version <= maxVersion) {
+		document.getElementById('compatibility').innerHTML=VERSION_CHECK_SUPPORTED.replace("%s", versionNumber);
 		document.body.style.setProperty("--title-color", "rgba(39,174,96, 0.5)");
-} else if (versionNumber > maxVersion) {
-		document.getElementById('compatibility').innerHTML=VERSION_CHECK_UNCONFIRMED.replace("%s", versionRepresentation);
+	} else if(versionNumber > maxVersion) {
+		document.getElementById('compatibility').innerHTML=VERSION_CHECK_UNCONFIRMED.replace("%s", versionNumber);
 		document.body.style.setProperty("--title-color", "rgba(192,57,43, 0.5)");
-} else if (!versionNumber) {
+	} else if(!versionNumber) {
 		document.getElementById('compatibility').innerHTML="<p>Can't get device version.</p>";
-document.body.style.setProperty("--title-color", "rgba(192,57,43, 0.5)");
-} else {
-		document.getElementById('compatibility').innerHTML=VERSION_CHECK_UNSUPPORTED.replace("%s", versionRepresentation);
+		document.body.style.setProperty("--title-color", "rgba(192,57,43, 0.5)");
+	} else {
+		document.getElementById('compatibility').innerHTML=VERSION_CHECK_UNSUPPORTED.replace("%s", versionNumber);
 		document.body.style.setProperty("--title-color", "rgba(192,57,43, 0.5)");
 	}
